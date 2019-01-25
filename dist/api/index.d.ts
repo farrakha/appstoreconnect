88c5b40e2ad3b09ba6bd0c5ebf33023fed2c1351
Ahmed Farrakha <farrakha@gmail.com>
2019-01-24 18:48:39 -0800

    build

---
+++ dist/api/index.d.ts
@@ -0,0 +1,45 @@
+import { ErrorResponse } from '../v1/error'
+export interface API {
+    baseUrl: string
+    token: string | null
+}
+export declare function makeAPI(baseUrl: string, token?: string): API
+export declare class APIError extends Error {
+    readonly response: ErrorResponse
+    constructor(response: ErrorResponse, code?: number, message?: string)
+}
+interface APIOptions {
+    query?: object
+    body?: object
+}
+export declare function HEAD<T>(
+    api: API,
+    path: string,
+    options?: APIOptions
+): Promise<T>
+export declare function GET<T>(
+    api: API,
+    path: string,
+    options?: APIOptions
+): Promise<T>
+export declare function POST<T>(
+    api: API,
+    path: string,
+    options?: APIOptions
+): Promise<T>
+export declare function PUT<T>(
+    api: API,
+    path: string,
+    options?: APIOptions
+): Promise<T>
+export declare function PATCH<T>(
+    api: API,
+    path: string,
+    options?: APIOptions
+): Promise<T>
+export declare function DELETE<T>(
+    api: API,
+    path: string,
+    options?: APIOptions
+): Promise<T>
+export {}
